6f780739561c8d44d4b534780bc180b2a6ddbb1a
Chhavi Khandelwal <chhavi@vinsol.com>
2013-09-05 16:54:27 +0530

    prototype function added

--- User-class/User-class.js
+++ User-class/User-class.js
@@ -1,17 +1,19 @@
 function User(name, age) {
 	this.name = name;
-	this.age = age;
-	this.compare = compare;   
+	this.age = age;  
 }
-function compare(user2) {
-	if (this.age == user2.age) {
+User.prototype = {
+	constructor : User,
+    compare : function(user2) {
+	if (this.age == user2.age) 
         alert (this.name + " and " + user2.name + " have the same age. ");
-	}
+	else
 	if (this.age > user2.age)
 		alert (this.name +" is older than " + user2.name);
 	else
 		alert (user2.name + " is older than " + this.name);
-}
+    }
+};
 var user1 = new User("John", 49);
 var user2 = new User("Mary", 40);
 user1.compare(user2);
