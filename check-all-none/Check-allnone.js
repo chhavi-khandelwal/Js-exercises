46708d5fc9e8776b709fc5cf73b03698fce07dcf
Chhavi Khandelwal <chhavi@vinsol.com>
2013-09-07 17:06:59 +0530

    object oriented approach used

--- check-all-none/Check-allnone.js
+++ check-all-none/Check-allnone.js
@@ -1,13 +1,14 @@
-function checkAllNone(check) {
-	var myform;
-	myform = document.getElementById("myform");
-	  for(var i=0, len=myform.childNodes.length; i < len; i++  )
-	  { if (myform.childNodes[i].type == "checkbox")
-		  if((check == true))
-		     myform.childNodes[i].checked = true;
-		  else
-		  if((check == false))
+function Form(id) {
+	this.id = id;
+}
+Form.prototype.checkAllOrNone = function(check) {
+	for(var i=0, len=myform.childNodes.length; i < len; i++  ) {
+	    if (myform.childNodes[i].type == "checkbox")
+		    if(check == true)
+		    myform.childNodes[i].checked = true;
+		    else
+		    if(check == false)
 			myform.childNodes[i].checked = false;
-	  }	
 	}
-	
+}
+var formObj = new Form("myform");
