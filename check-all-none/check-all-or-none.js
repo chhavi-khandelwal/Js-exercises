85ec953ac267d1b432c333a7470746eb2fc3088e
Chhavi Khandelwal <chhavi@vinsol.com>
2013-09-09 13:16:30 +0530

    corrected indentation

--- check-all-none/check-all-or-none.js
+++ check-all-none/check-all-or-none.js
@@ -1,10 +1,10 @@
-function Form(rootVal) {
-  this.root = document.getElementById(rootVal);
+function Form(id) {
+  this.root = document.getElementById(id);
 }
 Form.prototype.checkAllOrNone = function(check) {
   var checkboxes = document.querySelectorAll("#" + this.root.id + " input[type='checkbox']");
-  for (var i = 0, len = checkboxes.length; i < len; i++  ) {
+  for (var i = 0, len = checkboxes.length; i < len; i++) {
     checkboxes[i].checked = check;
-  }      
+  }
 }
 var formObj = new Form("myform");
