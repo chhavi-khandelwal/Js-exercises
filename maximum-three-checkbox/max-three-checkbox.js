424507a376481a9dc2f5bf368380398cf0961973
Chhavi Khandelwal <chhavi@vinsol.com>
2013-09-14 18:19:11 +0530

    code optimized

--- maximum-three-checkbox/max-three-checkbox.js
+++ maximum-three-checkbox/max-three-checkbox.js
@@ -1,30 +1,29 @@
-function Form(rootVal) {
-   this.root = document.getElementById(rootVal);
+function Form(id) {
+  this.root = document.getElementById(id);
+  this.none = document.getElementById("none");
+  this.checkboxes = document.querySelectorAll("#" + this.root.id + " input[type='checkbox'].days");
 }
 Form.prototype.checkMax = function(dayname) {
   var count = 0;
   var selectedDays = new Array();
-  var checkboxes = document.querySelectorAll("#" + this.root.id + " input[type='checkbox']");
-  for( var i = 0, len = checkboxes.length; i < len; i++) { 
-        if (checkboxes[i].checked && !(checkboxes[i] == dayname)) {
-            count++;  
-        }           
-        if (checkboxes[i].id == "none") 
-            checkboxes[i].checked = false;
-  }      
+  none.checked = false;
+  for ( var i = 0, len = this.checkboxes.length; i < len; i++) {
+    if(!(dayname == none)) {
+      if (this.checkboxes[i].checked && !(this.checkboxes[i] == dayname))
+        count++;
+    }
+    else {
+      none.checked = true;
+      this.checkboxes[i].checked = false;
+    }
+  }
   if (count >= 3) {
     dayname.checked = false;
-    for(var i = 0, len = checkboxes.length; i < len; i++) { 
-      if (checkboxes[i].checked)     
-      selectedDays.push(checkboxes[i].value);  
-    }  
-    alert("Only 3 days can be selected. You have already selected " + selectedDays[0] + "," + selectedDays[1] + " and " + selectedDays[2]);      
-  }
-}
-Form.prototype.deselect = function(dayname) {
-  var checkboxes = document.querySelectorAll("#" + this.root.id + " input[type='checkbox'] ");
-  for( var i = 0, len = checkboxes.length - 1; i < len; i++) {  
-        checkboxes[i].checked = false;
+    for (var i = 0, len = this.checkboxes.length; i < len; i++) {
+      if (this.checkboxes[i].checked)
+        selectedDays.push(this.checkboxes[i].value);
+    }
+    alert("Only 3 days can be selected. You have already selected " + selectedDays[0] + "," + selectedDays[1] + " and " + selectedDays[2]);
   }
 }
 var checkObj = new Form("myform");
