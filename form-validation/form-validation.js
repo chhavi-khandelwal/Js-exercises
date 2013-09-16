5ae1dc7e3aac8ba8e5cc88df0688e2f5943dc7a3
Chhavi Khandelwal <chhavi@vinsol.com>
2013-09-16 10:24:31 +0530

    review

--- form-validation/form-validation.js
+++ form-validation/form-validation.js
@@ -4,71 +4,60 @@ function Validation(id) {
 Validation.prototype.isFormValidate = function(validity) {
   if (validity == "filled") {
     if (this.element.id == "login" || this.element.id == "email" || this.element.id == "name" || this.element.id == "homepage") {
-      return this.notFilled(this.element.id);
+      var trimmedText = this.element.value.trim(); 
+      var message = this.element.id + " can't be empty";
+      if (trimmedText == "") {
+        alert(message);
+        return false;
+      }
+      else
+        return true;  
     }
   }
   if (validity == "minlength") {
-    if (this.element.id == "aboutme")
-      return this.minLength(this.element.id);
-  }
-  if (validity == "ischecked") {
-    if (this.element.id == "checkbox") {
-      return this.isChecked(this.element.id);
+    if (this.element.id == "aboutme") {
+      var textData = this.element.value.length;
+      var trimmedText = this.element.value.trim(); 
+      if (trimmedText == "" || trimmedText.length < 50) {
+        alert("Minimum length of text in " + this.element.id + " should be 50");
+        return false;
+      }
+      else
+        return true;
     }
   }
-}
-Validation.prototype.notFilled = function() {
-  var trimmedText = this.element.value.trim(); 
-  var message = this.element.id + " can't be empty";
-  if (trimmedText == "") {
-    alert(message);
-    return false;
+  if (validity == "ischecked") {
+    var confirmation;
+    if (this.element.id == "checkbox") {
+      if (!this.element.checked)
+        confirmation = confirm("Don't you want to receive the notification?");
+      else 
+        confirmation = confirm("Do you want to receive the notification?");
+    }
+        if(confirmation) 
+          return true; 
+        else 
+          return false;
   }
-  else
-    return true;  
-}
-Validation.prototype.minLength = function() {
-  var textData = this.element.value.length;
-  var trimmedText = this.element.value.trim(); 
-  if (trimmedText == "" || trimmedText.length < 50) {
-    alert("Minimum length of text in " + this.element.id + " should be 50");
-    return false;
-  }
-  else
-    return true;
-}
-Validation.prototype.isChecked = function() {
-  if (!this.element.checked) {
-    confirm("Don't you want to receive the notification?");
-    //return false;
-  }
-  else 
-    confirm("Do you want to receive the notification?");
 }
 var textboxes = document.getElementsByClassName("textboxdata");
 var field = new Array();
 for (var i = 0, len = textboxes.length; i < len; i++) {
   field[i] = new Validation(textboxes[i].id);
 }
-var textarea = document.getElementById("aboutme");
-area = new Validation(textarea.id);
-checkbox = document.getElementById("checkbox");
-checkNotify = new Validation(checkbox.id);
+var area = new Validation("aboutme");
+checkNotify = new Validation("checkbox");
 function formValidate(event) {
   for (var i = 0, len = textboxes.length; i < len; i++) {
-    if (!field[i].isFormValidate("filled", textboxes[i].id)) {
+    if (!field[i].isFormValidate("filled")) {
       event.preventDefault();
       return false;
     }
   }
-  if (!area.isFormValidate("minlength", textarea.id)) {
+  if (!area.isFormValidate("minlength"))
     event.preventDefault(); 
-    return false;
-  }
-  if (!checkNotify.isFormValidate("ischecked", checkbox.id)) {
-    return true;
-  }
+  if (!checkNotify.isFormValidate("ischecked"))
+    event.preventDefault();
 }
 var formId = document.getElementById("myform");
 formId.addEventListener("submit", formValidate, false);
-
