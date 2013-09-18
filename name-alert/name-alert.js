e61574f997b3bd8dcf382dab4cde586f74910493
Chhavi Khandelwal <chhavi@vinsol.com>
2013-09-18 18:38:19 +0530

    removed second argument, condition optimized, names modified

--- name-alert/name-alert.js
+++ name-alert/name-alert.js
@@ -1,19 +1,22 @@
-function userName(name) {
-  this.trueName = prompt("Enter your " + name);
-  this.trimmedName = this.trueName.trim();
-  if (!this.trimmedName) {
+function UserName(name) {
+  that = this;
+  this.myName = name;
+}
+UserName.prototype.alertName= function() {
+  var trueName = prompt("Enter your " + this.myName);
+  var trimmedName = trueName.trim();
+  if (!trimmedName) {
     alert("Enter the correct name");
-    return new userName(name);
+    return that.alertName();
   }
   else
-    return this.trueName; 
-  }
-userName.prototype.alertName = function(secondName) {
-  var message = "hello " + this.trueName + " " + secondName.trueName;
-  var div = document.getElementById("mydiv");
-  alert(message);
-  div.innerHTML = message;
-}
-var firstName = new userName("FirstName");
-var secondName = new userName("SecondName");
-firstName.alertName(secondName);
+    return trueName; 
+}  
+var firstName = new UserName("FirstName");
+var first = firstName.alertName();
+var secondName = new UserName("SecondName");
+var second = secondName.alertName();
+var message = "hello " + first + " " + second;
+var div = document.getElementById("mydiv");
+alert(message);
+div.innerHTML = message;
