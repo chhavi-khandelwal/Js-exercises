4f8d838992bbfa76f225b80738393e059ae1f113
Chhavi Khandelwal <chhavi@vinsol.com>
2013-09-20 18:27:53 +0530

    modularity added

--- form-validation/form-validation.js
+++ form-validation/form-validation.js
@@ -3,42 +3,45 @@ function Element(id) {
 }
 Element.prototype.isFormValidate = function(validity) {
   if (validity == "filled") {
-    if (this.element.id == "login" || this.element.id == "email" || this.element.id == "name" || this.element.id == "homepage") {
-      var trimmedText = this.element.value.trim(); 
-      var message = this.element.id + " can't be empty";
-      if (!trimmedText) {
-        alert(message);
-        return false;
-      }
-      else
-        return true;  
-    }
+    return this.isFilled();
   }
   if (validity == "minlength") {
-    if (this.element.id == "aboutme") {
-      var textData = this.element.value.length;
-      var trimmedText = this.element.value.trim(); 
-      if (!trimmedText || trimmedText.length < 50) {
-        alert("Minimum length of text in " + this.element.id + " should be 50");
-        return false;
-      }
-      else
-        return true;
-    }
+    return this.isMinLength();
   }
   if (validity == "ischecked") {
-    var confirmation;
-    if (this.element.id == "checkbox") {
-      if (!this.element.checked)
-        confirmation = confirm("Don't you want to receive the notification?");
-      else 
-        confirmation = confirm("Do you want to receive the notification?");
-    }
-        if(confirmation) 
-          return true; 
-        else 
-          return false;
+    return this.isChecked();
   }
+}
+Element.prototype.isFilled = function() {
+  var trimmedText = this.element.value.trim(); 
+  var message = this.element.id + " can't be empty";
+  if (!trimmedText) {
+    alert(message);
+    return false;
+  }
+  else
+    return true;  
+}
+Element.prototype.isMinLength = function() {
+  var textData = this.element.value.length;
+  var trimmedText = this.element.value.trim(); 
+  if (!trimmedText || trimmedText.length < 50) {
+    alert("Minimum length of text in " + this.element.id + " should be 50");
+    return false;
+  }
+  else
+    return true;
+}
+Element.prototype.isChecked = function() {
+  var confirmation;
+  if (!this.element.checked)
+    confirmation = confirm("Don't you want to receive the notification?");
+  else 
+    confirmation = confirm("Do you want to receive the notification?");
+  if(confirmation) 
+    return true; 
+  else 
+    return false;
 }
 var textboxes = document.getElementsByClassName("textboxdata");
 var field = new Array();
@@ -57,9 +60,10 @@ function formValidate(event) {
   if (!area.isFormValidate("minlength")) {
     event.preventDefault(); 
     return false;
-  }  
-  if (!checkNotify.isFormValidate("ischecked"))
+  }
+  if (!checkNotify.isFormValidate("ischecked")) {
     event.preventDefault();
+  }
 }
 var formId = document.getElementById("myform");
 formId.addEventListener("submit", formValidate, false);
