d18deedae4aaf622a360b2e84a2f69efa55f6b43
Chhavi Khandelwal <chhavi@vinsol.com>
2013-09-20 14:27:45 +0530

    added modularity

--- input-url/input-url.js
+++ input-url/input-url.js
@@ -2,15 +2,23 @@ function Url() {
   this.validUrl;
 }
 Url.prototype.openNewWindow = function() {
-  this.validUrl = prompt("Enter a URL");
-  var trimmedUrl = this.validUrl.trim();
+  do {
+    this.validUrl = prompt("Enter a URL");
+    var trimmedUrl = this.validUrl.trim();
+  } while (!this.validateUrl(trimmedUrl));
+    return this.windowOpen();     
+}
+Url.prototype.validateUrl = function(trimmedUrl) {
   if (!trimmedUrl) {
     alert("Enter a valid url");
-    return urlValue.openNewWindow();
+    return false;
   }
   else {
+    return true;
+  }  
+}
+Url.prototype.windowOpen = function() {
     window.open(this.validUrl,'',"width = 400, height = 450, scrollbars = 0, resizable = 0, location = 0, status = 0, menubar = 0");
-  }   
 }
 var urlValue = new Url();
 urlValue.openNewWindow();
