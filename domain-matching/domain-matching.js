29ca718d94edcd4cf7a95621f55516acb9e72ca1
Chhavi Khandelwal <chhavi@vinsol.com>
2013-09-21 18:01:25 +0530

    code optimized

--- domain-matching/domain-matching.js
+++ domain-matching/domain-matching.js
@@ -3,46 +3,19 @@ function Form(id) {
   this.formId.addEventListener("submit", this.alertUrl, false);
 }
 Form.prototype.alertUrl = function(event) {
-  var urlRegEx = /^(?:(?:http|https|ftp):\/\/)?(?:www\.)?((([\w\d\-\_]+\.)+(\w{2}\.)(\w{2}))|(([\w\d\-\_]+\.)+(\w{2,4})))(?:\/[\w\d\-\_\?\=\&\#\.]+)*$/i;
+  var urlRegEx = /^(?:(?:http|https|ftp):\/\/)?(?:www\.)?(?:((?:[\w\d\_\-]+\.)*[\w\d\-\_]+)\.)?([\w\d\_\-]+\.[\w\d]{2,4})(?:\/[\w\d\-\_\?\=\&\#\.]+)*$/i;
   var urlValue = document.getElementById("url").value;
   if (!urlRegEx.test(urlValue)) {
     alert("Enter a valid Url");
     event.preventDefault();
   }
   else {
-    var subdom = [];
-    var regexMatch1 = RegExp.$1;
-    var regexMatch2 = RegExp.$2;
-    var regexMatch3 = RegExp.$3;
-    var regexMatch4 = RegExp.$4;
-    var regexMatch5 = RegExp.$5;
-    var regexMatch6 = RegExp.$6;
-    var regexMatch7 = RegExp.$7;
-    var regexMatch8 = RegExp.$8;
-    var regexMatch9 = RegExp.$9;
-    var splitUrl = regexMatch1.split(/\./);  
-    if(!regexMatch2) {
-      if (splitUrl.length > 2) {
-        for (var i = 0; i < splitUrl.length - 2; i++) {
-        subdom.push(splitUrl[i]);
-        }
-        alert("domain name: " + regexMatch7 + regexMatch8 + " subdomain: " + subdom.join("."));
-      }
-      else {
-        alert("domain name: " + regexMatch7 + regexMatch8);
-      }
-    } 
-    else {
-      if (splitUrl.length > 3) {
-        for (var i=0; i < splitUrl.length - 3; i++) {
-        subdom.push(splitUrl[i]);
-        }
-        alert("domain name: " + regexMatch3 + regexMatch4 + regexMatch5 + " subdomain: " + subdom.join("."));
-      }  
-      else {
-        alert("domain name: " + regexMatch3 + regexMatch4 + regexMatch5);  
-      }
+    if (RegExp.$1) {
+      alert("domain name: " + RegExp.$2 + " subdomain: " + RegExp.$1);
     }
-  }  
+    else {
+      alert("domain name: " + RegExp.$2);
+    }
+  }
 }
 var form = new Form("myform");
