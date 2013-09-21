ee06e74ea3572af1aa7d2aa6b4209872b1ed3839
Chhavi Khandelwal <chhavi@vinsol.com>
2013-09-21 20:45:12 +0530

    removed innerHTML, code modularized and optimized

--- parent-children-checkbox/parent-children-checkbox.js
+++ parent-children-checkbox/parent-children-checkbox.js
@@ -1,33 +1,38 @@
 function Root(id, options) {
   this.root = document.getElementById(id);
   this.options = options;
+  this.checkedChild = this.openSubList(this.root);
 }
-Root.prototype.openList = function(option) {
-  var checkboxes = document.getElementsByClassName("parent-checkbox");
-  var checkChild = document.getElementsByClassName("checkboxlabel");
-  location.hash = checkChild[option].id;
-  if (checkboxes[option].checked) {        
-    var mainList = document.createElement("ul");
-    for (var j = 0; j < this.options.length; j++) {
-    mainList.id = option;
+Root.prototype.openList = function() {
+  location.hash = this.root.id;
+  this.checkedChild.style.display = this.root.checked ? "block" : "none";
+  this.checkChilds();
+}
+Root.prototype.checkChilds = function() {
+  var childCheckboxes = document.querySelectorAll("#" + this.checkedChild.id + " input[type='checkbox']");
+  for (var i = 0, len = childCheckboxes.length; i < len; i++) {
+    childCheckboxes[i].checked = this.root.checked; 
+  }
+}
+Root.prototype.openSubList = function(option) {
+  var mainList = document.createElement("ul");
+  mainList.id = "list" + option.value;
+  for (var j = 0; j < this.options.length; j++) {
     var list = document.createElement("li");
     list.style.listStyleType = "none";
     var checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     var label = document.createElement("label");
-    label.innerHTML = this.options[j];
+    var textNode = document.createTextNode(this.options[j]);
+    label.appendChild(textNode);
     list.appendChild(checkbox);
     list.appendChild(label);
     mainList.appendChild(list);
-   }  
-    checkChild[option].appendChild(mainList);
-    location.hash = checkChild[option].id;
   }
-  if (!checkboxes[option].checked) {
-  var unorderedList = document.getElementById(option);
-  checkChild[option].removeChild(unorderedList);        
-  }
-}   
+  option.parentNode.appendChild(mainList);
+  mainList.style.display = "none";
+  return mainList;
+}
 var drinks = new Array("COKE", "PEPSI", "DEW");
 var movies = new Array("DAR", "SIR");
 var bikes = new Array("HAYABUSA", "PULSAR", "CBZ");
