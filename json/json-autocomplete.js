e17a8a9c02c123fb07af0074076187b9ad5b2e5f
Chhavi Khandelwal <chhavi@vinsol.com>
2013-09-23 13:19:19 +0530

    changes done

--- json/json-autocomplete.js
+++ json/json-autocomplete.js
@@ -37,8 +37,8 @@ TextBox.prototype.autoComplete = function(event) {
   }  
   var suggestionNode = document.getElementsByClassName("suggestionNode");
   if (event.keyCode == 40 && that.count < suggestionNode.length) {
+    that.manageSuggestionList(that.count, suggestionNode);
     that.count++;
-    that.manageSuggestionList(that.count - 1, suggestionNode);
   }
   if (event.keyCode == 38 && that.count > 1) {
     --that.count;
@@ -46,34 +46,25 @@ TextBox.prototype.autoComplete = function(event) {
       that.manageSuggestionList(that.count - 1, suggestionNode);
     }
   }
-  if (event.keyCode == 8) {
+  if (event.keyCode == 8 && (that.textId.value)) {
     that.count = 0;
   }
   if (that.textId.value == "" || event.keyCode == 13) {
-    if (that.suggestionList) {
-      for (var i = 0, len = suggestionNode.length - 1; i < len; i++)
-        that.suggestionList.removeChild(suggestionNode[i]);
-      if (suggestionNode[0])
-        that.suggestionList.removeChild(suggestionNode[0]);  
-    }  
+    that.suggestionList.innerHTML = "";
   }
 }
 TextBox.prototype.manageSuggestionList = function(count, suggestionNode) {
   suggestionNode[count].style.backgroundColor = "#707070";
   that.textId.value = suggestionNode[count].innerHTML;
   for (var i = 0, len = suggestionNode.length; i < len; i++) {
-      if (i != count) 
-        suggestionNode[i].style.backgroundColor = "white";
-    }
+    if (i != count) 
+      suggestionNode[i].style.backgroundColor = "white";
+  }
 }
 TextBox.prototype.displaySuggestionList = function(enteredName) {
   that.suggestionList.innerHTML = "";
   var newArray = [];
-  for (var i = 0, len = jsonusers.length; i < len; i++) {
-    if (jsonText[i].user.toLowerCase().indexOf(enteredName) == 0) {
-      newArray.push(jsonText[i].user);
-    }
-  }
+  that.searchList(newArray, enteredName);
   for (var i = 0, len = newArray.length; i < len; i++) {
     var textNode = document.createTextNode(newArray[i]);
     if (!that.textId.value == "") {
@@ -83,5 +74,12 @@ TextBox.prototype.displaySuggestionList = function(enteredName) {
       that.suggestionList.appendChild(suggestion);
     }
   }  
+}
+TextBox.prototype.searchList = function(newArray, enteredName) {
+  for (var i = 0, len = jsonusers.length; i < len; i++) {
+    if (jsonText[i].user.toLowerCase().indexOf(enteredName) == 0) {
+      newArray.push(jsonText[i].user);
+    }
+  }
 }
 var autoObj = new TextBox("textbox");
