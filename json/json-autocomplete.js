7bb2c84cc817766a13296d9abfa4c7c4aee7f04e
Chhavi Khandelwal <chhavi@vinsol.com>
2013-09-25 13:03:47 +0530

    object scope changed, function calling corrected, code optimized

--- json/json-autocomplete.js
+++ json/json-autocomplete.js
@@ -30,60 +30,59 @@ function TextBox(id) {
   this.count = 0;
 }
 TextBox.prototype.autoComplete = function(event) {
-  var enteredName;
-  enteredName = that.textId.value.toLowerCase();
-  if (!that.textId.value == "" && event.keyCode != 40 && event.keyCode != 38) {
-    that.displaySuggestionList(enteredName);
+  if (event.keyCode != 40 && event.keyCode != 38) {
+    that.searchList();
   }  
   var suggestionNode = document.getElementsByClassName("suggestionNode");
   if (event.keyCode == 40 && that.count < suggestionNode.length) {
-    that.manageSuggestionList(that.count, suggestionNode);
     that.count++;
+    that.manageSuggestionList(suggestionNode);
   }
   if (event.keyCode == 38 && that.count > 1) {
     --that.count;
-    if (that.count < suggestionNode.length && !(that.count < 1)) {
-      that.manageSuggestionList(that.count - 1, suggestionNode);
-    }
+    that.manageSuggestionList(suggestionNode);
   }
-  if (event.keyCode == 8 && (that.textId.value)) {
+  if (event.keyCode == 8 && (that.textId.value)) {  //backspace
     that.count = 0;
   }
-  if (that.textId.value == "" || event.keyCode == 13) {
-    while (that.suggestionList.firstChild) {
-      that.suggestionList.removeChild(that.suggestionList.firstChild);
-    }
+  if (that.textId.value == "" || event.keyCode == 13) {  //enter key
+    that.removeSuggestionList();
   }
 }
-TextBox.prototype.manageSuggestionList = function(count, suggestionNode) {
-  suggestionNode[count].style.backgroundColor = "#707070";
-  that.textId.value = suggestionNode[count].textContent;
+TextBox.prototype.manageSuggestionList = function(suggestionNode) {
+  suggestionNode[this.count - 1].style.backgroundColor = "#707070";
+  this.textId.value = suggestionNode[this.count - 1].textContent;
   for (var i = 0, len = suggestionNode.length; i < len; i++) {
-    if (i != count) 
+    if (i != this.count - 1) 
       suggestionNode[i].style.backgroundColor = "white";
   }
 }
-TextBox.prototype.displaySuggestionList = function(enteredName) {
-  while (that.suggestionList.firstChild) {
-      that.suggestionList.removeChild(that.suggestionList.firstChild);
+TextBox.prototype.removeSuggestionList = function() {
+  while (this.suggestionList.firstChild) {
+    this.suggestionList.removeChild(this.suggestionList.firstChild);
   }
-  var newArray = [];
-  that.searchList(newArray, enteredName);
-  for (var i = 0, len = newArray.length; i < len; i++) {
-    var textNode = document.createTextNode(newArray[i]);
-    if (!that.textId.value == "") {
+}
+TextBox.prototype.displaySuggestionList = function(nameList) {
+  this.removeSuggestionList();
+  for (var i = 0, len = nameList.length; i < len; i++) {
+    var textNode = document.createTextNode(nameList[i]);
+    if (!this.textId.value == "") {
       var suggestion = document.createElement("div");
       suggestion.className = "suggestionNode";
       suggestion.appendChild(textNode);
-      that.suggestionList.appendChild(suggestion);
+      this.suggestionList.appendChild(suggestion);
     }
   }  
 }
-TextBox.prototype.searchList = function(newArray, enteredName) {
+TextBox.prototype.searchList = function() {
+  var enteredName;
+  enteredName = that.textId.value.toLowerCase();
+  var nameList = [];
   for (var i = 0, len = jsonusers.length; i < len; i++) {
     if (jsonText[i].user.toLowerCase().indexOf(enteredName) == 0) {
-      newArray.push(jsonText[i].user);
+      nameList.push(jsonText[i].user);
     }
   }
+  this.displaySuggestionList(nameList);
 }
 var autoObj = new TextBox("textbox");
